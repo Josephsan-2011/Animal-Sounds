fe084af8b115c78cbac6143900681ffc5aaf2f8d
Josephsan-2011 <83101904+Josephsan-2011@users.noreply.github.com>
2022-01-28 20:04:04 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 //https://teachablemachine.withgoogle.com/models/RsW6LvkUb/
+function start_classification() {
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier = ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/I_a2XpVfi/model.json",modelready)
+}
+function modelready() {
+    classifier.classify(got_results)
+}
+function got_results(error,results) {
+    console.log('got-result')
+}
